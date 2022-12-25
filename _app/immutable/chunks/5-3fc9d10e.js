390459096ba0bbcff1f5d7bca3253dfca24628e5
acheong08 <acheong@student.dalat.org>
2022-12-25 19:01:18 +0000

    Deploying to gh-pages from @ acheong08/track.duti.tech@83371c70790b8ddccb6c5418a47017159ad66493 🚀

---
+++ _app/immutable/chunks/5-3fc9d10e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/resources/chatgpt/_page.svelte-5685d9ac.js";export{t as component};
--- _app/immutable/components/pages/resources/chatgpt/_page.svelte-5685d9ac.js
+++ _app/immutable/components/pages/resources/chatgpt/_page.svelte-5685d9ac.js
@@ -1,4 +1,4 @@
-import{S as v,i as _,s as w,L as y,k as u,l as h,m as d,h as l,n as p,b as m,E as b,M as P,f,t as g,B as o,w as k,x as I,y as L,z as T,q as A,r as S}from"../../../../chunks/index-317a2139.js";import{S as O}from"../../../../chunks/SvelteMarkdown-8af5c757.js";const C=`# ChatGPT
+import{S as v,i as _,s as w,L as P,k as u,l as h,m as d,h as l,n as p,b as m,E as b,M as y,f,t as g,B as o,w as k,x as A,y as L,z as T,q as C,r as I}from"../../../../chunks/index-317a2139.js";import{S}from"../../../../chunks/SvelteMarkdown-8af5c757.js";const G=`# ChatGPT
 
 ChatGPT is a chatbot launched by OpenAI in November 2022. It is built on top of OpenAI's GPT-3.5 family of large language models, and is fine-tuned with both supervised and reinforcement learning techniques.
 
@@ -34,7 +34,16 @@ Probably the best local development library
 
 ### Features
 
-TODO
+- Captcha solving (paid)
+
+## LanLan69
+
+A newer Python development library for ChatGPT. A replacement for the old \`revChatGPT\`
+
+### Features
+
+- Fast captcha solving
+- Automated browser
 
 ## PawanOsman
 
@@ -43,4 +52,4 @@ TODO
 ## tonyb
 
 **WARNING**: **This is a paid service**
-`;function G(i){return{c:o,l:o,m:o,p:o,i:o,o,d:o}}function x(i){let e,a;return e=new O({props:{source:i[0]}}),{c(){k(e.$$.fragment)},l(n){I(e.$$.fragment,n)},m(n,t){L(e,n,t),a=!0},p:o,i(n){a||(f(e.$$.fragment,n),a=!0)},o(n){g(e.$$.fragment,n),a=!1},d(n){T(e,n)}}}function N(i){let e,a;return{c(){e=u("div"),a=A("Loading...")},l(n){e=h(n,"DIV",{});var t=d(e);a=S(t,"Loading..."),t.forEach(l)},m(n,t){m(n,e,t),b(e,a)},p:o,i:o,o,d(n){n&&l(e)}}}function $(i){let e,a,n,t={ctx:i,current:null,token:null,hasCatch:!1,pending:N,then:x,catch:G,value:0,blocks:[,,,]};return y(C,t),{c(){e=u("div"),a=u("article"),t.block.c(),this.h()},l(s){e=h(s,"DIV",{class:!0});var r=d(e);a=h(r,"ARTICLE",{class:!0});var c=d(a);t.block.l(c),c.forEach(l),r.forEach(l),this.h()},h(){p(a,"class","flex-auto prose max-w-screen-lg text-lg"),p(e,"class","container max-w-fit m-2")},m(s,r){m(s,e,r),b(e,a),t.block.m(a,t.anchor=null),t.mount=()=>a,t.anchor=null,n=!0},p(s,[r]){i=s,P(t,i,r)},i(s){n||(f(t.block),n=!0)},o(s){for(let r=0;r<3;r+=1){const c=t.blocks[r];g(c)}n=!1},d(s){s&&l(e),t.block.d(),t.token=null,t=null}}}class q extends v{constructor(e){super(),_(this,e,null,$,w,{})}}export{q as default};
+`;function O(i){return{c:o,l:o,m:o,p:o,i:o,o,d:o}}function x(i){let e,a;return e=new S({props:{source:i[0]}}),{c(){k(e.$$.fragment)},l(n){A(e.$$.fragment,n)},m(n,t){L(e,n,t),a=!0},p:o,i(n){a||(f(e.$$.fragment,n),a=!0)},o(n){g(e.$$.fragment,n),a=!1},d(n){T(e,n)}}}function N(i){let e,a;return{c(){e=u("div"),a=C("Loading...")},l(n){e=h(n,"DIV",{});var t=d(e);a=I(t,"Loading..."),t.forEach(l)},m(n,t){m(n,e,t),b(e,a)},p:o,i:o,o,d(n){n&&l(e)}}}function $(i){let e,a,n,t={ctx:i,current:null,token:null,hasCatch:!1,pending:N,then:x,catch:O,value:0,blocks:[,,,]};return P(G,t),{c(){e=u("div"),a=u("article"),t.block.c(),this.h()},l(s){e=h(s,"DIV",{class:!0});var r=d(e);a=h(r,"ARTICLE",{class:!0});var c=d(a);t.block.l(c),c.forEach(l),r.forEach(l),this.h()},h(){p(a,"class","flex-auto prose max-w-screen-lg text-lg"),p(e,"class","container max-w-fit m-2")},m(s,r){m(s,e,r),b(e,a),t.block.m(a,t.anchor=null),t.mount=()=>a,t.anchor=null,n=!0},p(s,[r]){i=s,y(t,i,r)},i(s){n||(f(t.block),n=!0)},o(s){for(let r=0;r<3;r+=1){const c=t.blocks[r];g(c)}n=!1},d(s){s&&l(e),t.block.d(),t.token=null,t=null}}}class q extends v{constructor(e){super(),_(this,e,null,$,w,{})}}export{q as default};
